a39de0abb0a952c0cc7b158b2e994b1477b9533e
jonmatthis <jonmatthis@gmail.com>
2023-08-03 11:46:17 -0400

    downloading audio successful

--- jonbot/layer0_frontends/discord_bot/discord_client.py
+++ jonbot/layer0_frontends/discord_bot/discord_client.py
@@ -1,11 +1,12 @@
 import logging
+import os
 import uuid
 
 import aiohttp
 import discord
+import requests
 
-
-from jonbot.layer1_api_interface.app import API_CHAT_URL
+from jonbot.layer1_api_interface.app import API_CHAT_URL, API_VOICE_TO_TEXT_URL
 from jonbot.layer3_data_layer.data_models.conversation_models import ChatInput, ChatResponse
 from jonbot.layer3_data_layer.database.mongo_database import mongo_database_manager
 
@@ -30,27 +31,42 @@ async def on_message(message: discord.Message) -> None:
     if message.author == discord_client.user:
         return
 
+
+
+
     try:
         async with aiohttp.ClientSession() as session:
-            chat_input = ChatInput(message=message.content,
-                                   uuid=str(uuid.uuid4()),
-                                   )
-            logger.info(f"Sending chat input: {chat_input}")
-            async with session.post(API_CHAT_URL, json=chat_input.dict()) as response:
-                if response.status == 200:
-                    data = await response.json()
-                    chat_response = ChatResponse(**data)
-                    await message.channel.send(chat_response.message)
-                    mongo_database_manager.insert_discord_message(message = message, bot_response = chat_response)
-                else:
-                    error_message = f"Received non-200 response code: {response.status}"
-                    logger.info(error_message)
-                    await message.channel.send(
-                        f"Sorry, I'm currently unable to process your request. {error_message}")
+
+            if len(message.attachments) > 0 and message.attachments[0].content_type.startswith("audio"):
+
+                async with session.post(API_VOICE_TO_TEXT_URL, json={"audio_file_url":message.attachments[0].url}) as response:
+                    if response.status == 200:
+                        transcribed_text = response.text
+                        await message.channel.send(transcribed_text)
+                    else:
+                        await message.channel.send("Sorry, I was unable to transcribe the audio.")
+
+            else:
+
+                chat_input = ChatInput(message=message.content,
+                                       uuid=str(uuid.uuid4()),
+                                       )
+                logger.info(f"Sending chat input: {chat_input}")
+                async with session.post(API_CHAT_URL, json=chat_input.dict()) as response:
+                    if response.status == 200:
+                        data = await response.json()
+                        chat_response = ChatResponse(**data)
+                        await message.channel.send(chat_response.message)
+                        mongo_database_manager.insert_discord_message(message = message, bot_response = chat_response)
+                    else:
+                        error_message = f"Received non-200 response code: {response.status}"
+                        logger.exception(error_message)
+                        await message.channel.send(
+                            f"Sorry, I'm currently unable to process your request. {error_message}")
 
     except Exception as e:
         error_message = f"An error occurred: {str(e)}"
-        logger.info(error_message)
+        logger.exception(error_message)
         await message.channel.send(f"Sorry, an error occurred while processing your request. {error_message}")
 
 
--- jonbot/layer1_api_interface/app.py
+++ jonbot/layer1_api_interface/app.py
@@ -1,8 +1,12 @@
 import asyncio
 import logging
+import os
 import time
-
-from fastapi import FastAPI
+import uuid
+import aiofiles
+import aiohttp
+from fastapi import FastAPI, UploadFile, File
+from pydantic import BaseModel
 
 from jonbot.layer2_core_processes.controller.controller import Controller
 from jonbot.layer3_data_layer.data_models.conversation_models import ChatInput, ChatResponse
@@ -10,6 +14,7 @@ from jonbot.layer3_data_layer.data_models.conversation_models import ChatInput, 
 logger = logging.getLogger(__name__)
 
 API_CHAT_URL = "http://localhost:8000/chat"
+API_VOICE_TO_TEXT_URL = "http://localhost:8000/voice_to_text"
 
 app = FastAPI()
 controller = asyncio.run(Controller.initialize())
@@ -27,7 +32,24 @@ async def chat(chat_input: ChatInput) -> ChatResponse:
     logger.info(f"Returning chat response: {response}, elapsed time: {toc - tic:0.4f} seconds")
     return response
 
+class VoiceToTextRequest(BaseModel):
+    audio_file_url: str
+@app.post("/voice_to_text")
+async def voice_to_text(request: VoiceToTextRequest) -> str:
+    audio_file_url = request.audio_file_url
+    file_path = f"/tmp/voice-message.ogg"  # Temporary file path to store the audio
 
+    async with aiohttp.ClientSession() as session:
+        async with session.get(audio_file_url) as response:
+            if response.status == 200:
+                # Async file writing
+                async with aiofiles.open(file_path, mode='wb') as file:
+                    await file.write(await response.read())
+                print("File downloaded successfully.")
+            else:
+                print("Failed to download file.")
+
+    return "wow"
 def run_api():
     """
     Run the API for JonBot
