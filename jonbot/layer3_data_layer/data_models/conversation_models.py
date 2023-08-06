36ca71edf89285c1f04a599635dedc70351a521e
jonmatthis <jonmatthis@gmail.com>
2023-08-06 09:48:44 -0400

    del old conversation/applications models

--- jonbot/layer3_data_layer/data_models/conversation_models.py
+++ jonbot/layer3_data_layer/data_models/conversation_models.py
@@ -4,10 +4,12 @@ from typing import Any
 from pydantic import BaseModel, Field
 
 
+
 class ChatInput(BaseModel):
     message: str
     uuid: str = Field(default_factory=lambda: str(uuid.uuid4()))
     metadata: dict = {}
+
 
 
 
@@ -23,18 +25,3 @@ class ChatResponse(BaseModel):
     metadata: dict = {}
 
 
-class ChatInteraction(BaseModel):
-    human_input: ChatInput
-    bot_response: ChatResponse
-    uuid: str = Field(default_factory=lambda: str(uuid.uuid4()))
-
-    class Config:
-        arbitrary_types_allowed = True
-
-
-class ConversationModel(BaseModel):
-    interactions: list[ChatInteraction] = []
-    uuid: str = Field(default_factory=lambda: str(uuid.uuid4()))
-
-    def model_post_init(self, __context: Any) -> None:
-        self.uuid = str(uuid.uuid4())
