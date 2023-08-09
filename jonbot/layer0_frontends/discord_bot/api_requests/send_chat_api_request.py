c4b058ee26a4cfe5f11c8d63042587517135b0e9
jonmatthis <jonmatthis@gmail.com>
2023-08-09 09:27:14 -0400

    add something to handle long messages

--- jonbot/layer0_frontends/discord_bot/api_requests/send_chat_api_request.py
+++ jonbot/layer0_frontends/discord_bot/api_requests/send_chat_api_request.py
@@ -12,9 +12,28 @@ async def send_chat_api_request(api_route:str,
     logger.info(f"Sending chat request payload: {chat_request.dict()}")
     response = await send_request_to_api(api_route=api_route, data=chat_request.dict())
     chat_response = ChatResponse(**response)
-    await message.reply(chat_response.message)
+    await send_chat_response_reply(chat_response, message)
     logger.info(f"ChatRequest payload sent: \n {chat_request.dict()}\n "
                 f"ChatResponse payload received: \n {chat_response.dict()}"
                 f"Successfully sent chat request payload to API!")
+
+
+async def send_chat_response_reply(chat_response:ChatResponse,
+                                   message:discord.Message,
+                                   max_message_length:int=2000):
+    comfy_message_length = int(max_message_length*.9)
+    if len(chat_response.text) > comfy_message_length:
+        await handle_long_message(chat_response, max_message_length, message)
+    else:
+        await message.reply(chat_response.text)
+
+
+async def handle_long_message(chat_response, max_message_length, message):
+    message_chunks = [chat_response.text[index:index + max_message_length] for index in
+                      range(0, len(chat_response.text), max_message_length)]
+    for chunk_number, message_chunk in enumerate(message_chunks):
+        message = await message.reply(f"`Message {chunk_number + 1} out of {len(message_chunks)}` {message_chunk}")
+
+
 
 
