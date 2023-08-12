fd15c95578eb959050dd1ec440f8d83e02092113
jonmatthis <jonmatthis@gmail.com>
2023-08-12 11:55:26 -0400

    quine upgrade!

--- utilities/quine.py
+++ utilities/quine.py
@@ -1,127 +1,152 @@
+import datetime
+import logging
 import os
-import platform
 import subprocess
-from datetime import datetime
 from pathlib import Path
-from typing import List
+import platform
+
+from pydantic import BaseModel
+from typing import List, Optional, Literal
+import ast
+
+logger  = logging.getLogger(__name__)
+
+class QuineConfig(BaseModel):
+    base_directory: str
+    excluded_directories: List[str]
+    included_extensions: List[str]
+    excluded_file_names: List[str]
+    print_content_for: List[str]
+    print_mode: Literal["editor", "terminal", "both"] = "both"
+    output_file_name: Optional[str] = f"quine_{datetime.datetime.now().isoformat().replace(':', '-')}.md"
+    output_directory: str = str(Path(__file__).parent / "quine_output")
+
+    def output_file_path(self) -> str:
+        Path(self.output_directory).mkdir(parents=True, exist_ok=True)
+        return str(Path(self.output_directory) / self.output_file_name)
 
 
-class Quine:
-    """
-    A class to represent a Quine.
 
-    A Quine is a computer program which takes no input and produces a copy of its own source code as its only output.
-    In this case, the term "Quine" is used a bit more loosely to represent a program that prints out the source code
-    in a directory. This can be useful for various purposes, such as feeding the code through a Language-to-Language
-    Migration (LLM) tool.
+class StructurePrinter:
+    def __init__(self, config: QuineConfig):
+        self.config = config
 
-    ...
-
-    Attributes
-    ----------
-    base_directory : str
-        the base directory to start from
-    excluded_directories : set
-        a set of directories to be excluded
-    included_extensions : set
-        a set of file extensions to be excluded
-
-    Methods
-    -------
-    write_to_file(root_directory:str, file_name:str, output_file:object):
-        Writes the content of the file to the output markdown file.
-    generate_quine():
-        Walks through the base directory and its subdirectories, excluding certain directories and file types,
-        and writes the Python source code to a Markdown file with headings for each folder and subheadings for each file.
-        The source code is enclosed in ```python ``` code blocks.
-    """
-
-    def __init__(self, base_directory: str, excluded_directories: List[str], included_extensions: List[str],
-                 excluded_file_names: List[str]):
-        self.output_file_path = None
-        self.base_directory = base_directory
-        self.excluded_directories = excluded_directories
-        self.included_extensions = included_extensions
-        self.excluded_file_names = excluded_file_names
-
-    def write_to_file(self, root_directory: str, file_name: str, output_file: object) -> None:
+    def _parse_file(self, file_path: str) -> List[str]:
         """
-        Writes the content of the file to the output markdown file.
+        Parse the Python file to extract classes, functions, and constants.
 
-        Parameters
-        ----------
-        root_directory : str
-            The root directory.
-        file_name : str
-            The file name.
-        output_file : object
-            The output file object.
-        """
-        output_file.write("+++python\n")
-        output_file.write(f"## {file_name}\n\n")
-        try:
-            with open(os.path.join(root_directory, file_name), "r",
-                      encoding="utf-8") as file:  # specifying utf-8 encoding
-                file_content = file.read()
-                output_file.write(file_content)
-                print(file_content)  # Print the file content to the terminal
-        except UnicodeDecodeError as e:
-            print(f"Failed to read the file {file_name} due to encoding issues: {e}")
-        output_file.write("\n+++\n\n")
+        Args:
+            file_path (str): Path to the file to parse.
 
-    def generate_quine(self) -> None:
+        Returns:
+            List[str]: List of entity names in the file.
         """
-        Walks through the base directory and its subdirectories, excluding certain directories and file types,
-        and writes the Python source code to a Markdown file with headings for each folder and subheadings for each file.
-        The source code is enclosed in ```python ``` code blocks.
+        with open(file_path, 'r') as file:
+            node = ast.parse(file.read())
+
+        entities = []
+        for item in node.body:
+            if isinstance(item, (ast.FunctionDef, ast.AsyncFunctionDef)):
+                entities.append(f"Function: {item.name}")
+            elif isinstance(item, ast.ClassDef):
+                entities.append(f"Class: {item.name}")
+            elif isinstance(item, ast.Assign):
+                for target in item.targets:
+                    if isinstance(target, ast.Name):
+                        entities.append(f"Constant: {target.id}")
+
+        return entities
+
+    def print_structure(self):
         """
-        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        file_name = f"quine_{current_time}.md"
-        output_dir = Path().cwd() / "output"  # Output directory
-        output_dir.mkdir(parents=True, exist_ok=True)  # Create the output directory if it doesn't exist
-        file_path = output_dir / file_name  # Output file path
-        self.output_file_path = str(file_path)  # Store the output file path
-        with open(file_path, "w", encoding="utf-8") as output_file:  # specifying utf-8 encoding
-            for root_directory, directories, files in os.walk(self.base_directory):
-                directories[:] = [directory for directory in directories if directory not in self.excluded_directories]
-                if root_directory != ".":
-                    output_file.write(f"# {os.path.relpath(root_directory, '..')}\n\n")
-                for file_name in files:
-                    if file_name in self.excluded_file_names:
-                        continue
-                    if any(file_name.endswith(extension) for extension in self.included_extensions):
-                        self.write_to_file(root_directory, file_name, output_file)
+        Print the structure of the package based on the configuration.
+        """
+        for root_directory, directories, files in os.walk(self.config.base_directory):
+            directories[:] = [d for d in directories if d not in self.config.excluded_directories]
+
+            # Print directory name
+            print(f"Directory: {root_directory}")
+
+            for file_name in files:
+                if file_name in self.config.excluded_file_names:
+                    continue
+                if any(file_name.endswith(extension) for extension in self.config.included_extensions):
+                    file_path = os.path.join(root_directory, file_name)
+                    entities = self._parse_file(file_path)
+                    print(f"  File: {file_name}")
+                    for entity in entities:
+                        print(f"    {entity}")
+
+
+class ContentPrinter:
+    def __init__(self, config: QuineConfig):
+        self.config = config
+
 
     def open_file(self) -> None:
-        """
-        Opens the generated file in the system's default program associated with the file's type.
-        """
-
+        """Opens the generated file in the system's default program associated with the file's type."""
         current_os = platform.system()  # Get the current operating system
 
         try:
             if current_os == "Windows":
-                os.startfile(self.output_file_path)
+                os.startfile(self.config.output_file_path())
             elif current_os == "Darwin":  # MacOS
-                subprocess.run(("open", self.output_file_path), check=True)
+                subprocess.run(("open", self.config.output_file_path()), check=True)
             elif current_os == "Linux":
-                subprocess.run(("xdg-open", self.output_file_path), check=True)
+                subprocess.run(("xdg-open", self.config.output_file_path()), check=True)
             else:
                 print(f"Unsupported operating system: {current_os}")
         except Exception as e:
             print(f"Failed to open the file: {e}")
 
+    def print_content(self):
+        for content_path in self.config.print_content_for:
+            if os.path.isdir(content_path):
+                # If the provided path is a directory, iterate over all files in the directory
+                for file_name in os.listdir(content_path):
+                    file_path = os.path.join(content_path, file_name)
+                    if file_path.endswith(tuple(self.config.included_extensions)):
+                        self._print_file_content(file_path)
+            else:
+                self._print_file_content(content_path)
+
+    def _print_file_content(self, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                content = file.read()
+
+            if self.config.print_mode in ["terminal", "both"]:
+                print(f"Content of {file_path}:\n{'='*40}\n")
+                print(content)
+
+            # Save the content to the specified output file
+            with open(self.config.output_file_path(), 'w') as file:
+                file.write(content)
+
+            if self.config.print_mode in ["editor", "both"]:
+                self.open_file()
+
+        except Exception as e:
+            print(f"Error processing {file_path}: {e}")
+class Quine:
+    def __init__(self, config: QuineConfig):
+        self.config = config
+        self.structure_printer = StructurePrinter(config)
+        self.content_printer = ContentPrinter(config)
+
+    def generate(self):
+        self.structure_printer.print_structure()
+        self.content_printer.print_content()
+
 
 if __name__ == "__main__":
     base_directory_in = r"C:\Users\jonma\github_repos\jonmatthis\jonbot\jonbot\layer1_api_interface\api"
-    quine = Quine(
+    quine_config = QuineConfig(
         base_directory=base_directory_in,
-        excluded_directories=["__pycache__",
-                              ".git",
-                              "legacy",
-                              ],
-        included_extensions=[".py", ".html", ".js", ".css", ".md", ".json", ".csv", ".txt"],
-        excluded_file_names=["poetry.lock", ".gitignore", "LICENSE"]
+        excluded_directories=["__pycache__", ".git", "legacy"],
+        included_extensions=[".py"],
+        excluded_file_names=["poetry.lock", ".gitignore", "LICENSE"],
+        print_content_for=[r"C:\Users\jonma\github_repos\jonmatthis\jonbot\utilities\quine.py"]
     )
-    quine.generate_quine()
-    quine.open_file()
+    quine = Quine(quine_config)
+    quine.generate()
