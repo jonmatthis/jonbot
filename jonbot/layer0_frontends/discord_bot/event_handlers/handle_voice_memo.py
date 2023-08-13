dc219dc901e93e61a328520e63d4ee44fa45fa84
jonmatthis <jonmatthis@gmail.com>
2023-08-12 21:23:31 -0400

    voice memo work

--- jonbot/layer0_frontends/discord_bot/event_handlers/handle_voice_memo.py
+++ jonbot/layer0_frontends/discord_bot/event_handlers/handle_voice_memo.py
@@ -18,14 +18,12 @@ async def handle_voice_memo(message: discord.Message):
 
     voice_to_text_request = VoiceToTextRequest(audio_file_url=message.attachments[0].url)
 
-    await send_voice_to_text_api_request(api_route=api_client.get_api_endpoint_url(VOICE_TO_TEXT_ENDPOINT),
-                                         voice_to_text_request=voice_to_text_request,
+    await send_voice_to_text_api_request(voice_to_text_request=voice_to_text_request,
                                          message=message
                                          )
 
 
-async def send_voice_to_text_api_request(api_route: str,
-                                         voice_to_text_request: VoiceToTextRequest,
+async def send_voice_to_text_api_request(voice_to_text_request: VoiceToTextRequest,
                                          message: discord.Message):
     logger.info(f"Sending voice to text request payload: {voice_to_text_request.dict()}")
     reply_message = await message.reply("`awaiting bot response...`")
