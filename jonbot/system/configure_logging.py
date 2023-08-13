cc3fed55e35f09d22b80203f7cc5c48de5852a7f
jonmatthis <jonmatthis@gmail.com>
2023-08-12 20:21:19 -0400

    logging so pretty

--- jonbot/system/configure_logging.py
+++ jonbot/system/configure_logging.py
@@ -1,4 +1,4 @@
-import io
+import inspect
 import logging
 import logging.handlers
 import sys
@@ -11,22 +11,40 @@ from jonbot.system.path_getters import get_base_data_folder_path, LOG_FILE_FOLDE
 DEFAULT_LOGGING = {"version": 1, "disable_existing_loggers": False}
 
 LOG_FILE_PATH = None
+TRACE_LEVEL = 5
+logging.addLevelName(TRACE_LEVEL, "TRACE")
+logging.root.setLevel(TRACE_LEVEL)
 
-format_string = "[%(asctime)s.%(msecs)04d] [%(levelname)8s] [%(name)s] [%(funcName)s():%(lineno)s] [PID:%(process)d TID:%(thread)d] %(message)s"
 
-default_logging_formatter = logging.Formatter(fmt=format_string, datefmt="%Y-%m-%dT%H:%M:%S")
+# Add the method for the new TRACE level
+def trace(self, message, *args, **kws):
+    if self.isEnabledFor(TRACE_LEVEL):
+        self._log(TRACE_LEVEL, message, args, **kws)
+
+logging.Logger.trace = trace
 
 
-def get_logging_handlers(entry_point: str = None):
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        # You can add custom attributes here, like:
+        frame = inspect.stack()[8]
+        record.caller_name = frame[1]
+        record.caller_funcName = frame[3]
+        record.caller_lineno = frame[2]
+        return super().format(record)
+
+# Then, use the custom formatter:
+format_string = "[%(asctime)s.%(msecs)04d] [%(levelname)8s] [%(caller_name)s] [%(caller_funcName)s():%(caller_lineno)s] [PID:%(process)d TID:%(thread)d] %(message)s"
+custom_formatter = CustomFormatter(fmt=format_string, datefmt="%Y-%m-%dT%H:%M:%S")
+
+
+def get_logging_handlers():
     dictConfig(DEFAULT_LOGGING)
-
     console_handler = build_console_handler()
     file_handler = build_file_handler()
 
-    if not entry_point == "cli":
-        return [console_handler, file_handler]
-
-    return [file_handler]
+    return [file_handler,
+            console_handler]
 
 
 def get_log_file_path():
@@ -38,29 +56,67 @@ def get_log_file_path():
 
 def build_file_handler():
     file_handler = logging.FileHandler(get_log_file_path(), encoding="utf-8")
-    file_handler.setFormatter(default_logging_formatter)
-    file_handler.setLevel(logging.DEBUG)
+    file_handler.setLevel(TRACE_LEVEL)
+    file_handler.setFormatter(custom_formatter)
     return file_handler
 
 
+class ColoredConsoleHandler(logging.StreamHandler):
+    COLORS = {
+        "TRACE": "\033[37m",  # Dark White (equivalent to gray)
+        "DEBUG": "\033[36m",  # Dark Cyan
+        "INFO": "\033[34m",  # Blue
+        "WARNING": "\033[33m",  # YELLOW
+        "ERROR": "\033[31m",  # RED
+        "CRITICAL": "\033[41m",  # BG RED
+    }
+
+    def emit(self, record):
+        # Set the color for this record
+        color_code = self.COLORS.get(record.levelname, "\033[0m")
+
+        # Format the record with color
+        formatted_record = color_code + self.format(record) + "\033[0m"  # \033[0m resets the color
+
+        # Print the colored record to console
+        print(formatted_record)
+
+
 def build_console_handler():
-    stream = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-    console_handler = logging.StreamHandler(stream)
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(default_logging_formatter)
+    console_handler = ColoredConsoleHandler(stream=sys.stdout)
+    console_handler.setLevel(TRACE_LEVEL)
+    console_handler.setFormatter(custom_formatter)
     return console_handler
 
 
-def configure_logging(entry_point: str = None):
+def configure_logging():
     print(f"Setting up logging  {__file__}")
 
     if len(logging.getLogger().handlers) == 0:
-        handlers = get_logging_handlers(entry_point=entry_point)
+        handlers = get_logging_handlers()
         for handler in handlers:
             if not handler in logging.getLogger("").handlers:
                 logging.getLogger("").handlers.append(handler)
-
-        logging.root.setLevel(logging.DEBUG)
     else:
         logger = logging.getLogger(__name__)
         logger.info("Logging already configured")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # configure the logging
+    configure_logging()
+    logger = logging.getLogger(__name__)
+    # log some test messages
+    logger.trace("This is a TRACE message.")
+    logger.debug("This is a DEBUG message.")
+    logger.info("This is an INFO message.")
+    logger.warning("This is a WARNING message.")
+    logger.error("This is an ERROR message.")
+    logger.critical("This is a CRITICAL message.")
+
+    print("Logging setup and tests completed. Check the console output and the log file.")
