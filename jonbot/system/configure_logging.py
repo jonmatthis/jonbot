1f5a0f128a7a5105e676d2730818dd23dd038ace
jonmatthis <jonmatthis@gmail.com>
2023-08-16 07:21:02 -0400

    clean up logging setup - add `success` level

--- jonbot/system/configure_logging.py
+++ jonbot/system/configure_logging.py
@@ -1,94 +1,97 @@
-import inspect
 import logging
-import logging.handlers
 import sys
+from enum import Enum
 from logging.config import dictConfig
 
 from jonbot.system.path_getters import get_log_file_path
 
-DEFAULT_LOGGING = {"version": 1, "disable_existing_loggers": False}
 
-format_string = "[%(asctime)s.%(msecs)04d] [%(levelname)8s] [%(name)s] [%(funcName)s():%(lineno)s] [PID:%(process)d TID:%(thread)d] %(message)s"
-default_logging_formatter = logging.Formatter(fmt=format_string, datefmt="%Y-%m-%dT%H:%M:%S")
-
-LOG_FILE_PATH = None
-TRACE = 5
-logging.addLevelName(TRACE, "TRACE")
-
-# logging.root.setLevel(TRACE)
-logging.root.setLevel(logging.DEBUG)
-# logging.root.setLevel(logging.INFO)
+class LogLevel(Enum):
+    TRACE = 5
+    DEBUG = logging.DEBUG  # 10
+    INFO = logging.INFO  # 20
+    SUCCESS = 25
+    WARNING = logging.WARNING  # 30
+    ERROR = logging.ERROR  # 40
+    CRITICAL = logging.CRITICAL  # 50
 
 
-# Add the method for the new TRACE level
-def trace(self, message, *args, **kws):
-    if self.isEnabledFor(TRACE):
-        self._log(TRACE, message, args, **kws)
+logging.addLevelName(LogLevel.TRACE.value, "TRACE")
+logging.addLevelName(LogLevel.SUCCESS.value, "SUCCESS")
 
 
-logging.Logger.trace = trace
+class LoggerBuilder:
+    DEFAULT_LOGGING = {"version": 1, "disable_existing_loggers": False}
+    format_string = ("[%(asctime)s.%(msecs)04d] [%(levelname)8s] [%(name)s] "
+                     "[%(funcName)s():%(lineno)s] [PID:%(process)d TID:%(thread)d] %(message)s")
+
+    def __init__(self, level: LogLevel):
+        self.default_logging_formatter = logging.Formatter(
+            fmt=self.format_string, datefmt="%Y-%m-%dT%H:%M:%S")
+        dictConfig(self.DEFAULT_LOGGING)
+        self._add_trace_method_to_logger()
+        self._add_success_method_to_logger()
+        self._set_logging_level(level)
+
+    def _set_logging_level(self, level: LogLevel):
+        logging.root.setLevel(level.value)
+
+    def _add_trace_method_to_logger(self):
+        def trace(self, message, *args, **kws):
+            if self.isEnabledFor(LogLevel.TRACE.value):
+                self._log(LogLevel.TRACE.value, message, args, **kws)
+
+        logging.Logger.trace = trace
+
+    def _add_success_method_to_logger(self):
+        def success(self, message, *args, **kws):
+            if self.isEnabledFor(LogLevel.SUCCESS.value):
+                self._log(LogLevel.SUCCESS.value, message, args, **kws)
+
+        logging.Logger.success = success
+
+    def build_file_handler(self):
+        file_handler = logging.FileHandler(get_log_file_path(), encoding="utf-8")
+        file_handler.setLevel(LogLevel.TRACE.value)
+        file_handler.setFormatter(self.default_logging_formatter)
+        return file_handler
+
+    class ColoredConsoleHandler(logging.StreamHandler):
+        COLORS = {
+            "TRACE": "\033[37m",  # Dark White (grey)
+            "DEBUG": "\033[34m",  # Blue
+            "INFO": "\033[96m",  # Cyan
+            "SUCCESS": "\033[95m",  # Magenta
+            "WARNING": "\033[33m",  # Yellow
+            "ERROR": "\033[31m",  # Red
+            "CRITICAL": "\033[41m",  # Background Red
+        }
+
+        def emit(self, record):
+            color_code = self.COLORS.get(record.levelname, "\033[0m")
+            formatted_record = color_code + self.format(record) + "\033[0m"
+            print(formatted_record)
+
+    def build_console_handler(self):
+        console_handler = self.ColoredConsoleHandler(stream=sys.stdout)
+        console_handler.setLevel(LogLevel.TRACE.value)
+        console_handler.setFormatter(self.default_logging_formatter)
+        return console_handler
+
+    def configure(self):
+        if len(logging.getLogger().handlers) == 0:
+            handlers = [self.build_file_handler(), self.build_console_handler()]
+            for handler in handlers:
+                if handler not in logging.getLogger("").handlers:
+                    logging.getLogger("").handlers.append(handler)
+        else:
+            logger = logging.getLogger(__name__)
+            logger.info("Logging already configured")
 
 
-
-
-
-def get_logging_handlers():
-    dictConfig(DEFAULT_LOGGING)
-    console_handler = build_console_handler()
-    file_handler = build_file_handler()
-
-    return [file_handler,
-            console_handler]
-
-
-
-
-def build_file_handler():
-    file_handler = logging.FileHandler(get_log_file_path(), encoding="utf-8")
-    file_handler.setLevel(TRACE)
-    file_handler.setFormatter(default_logging_formatter)
-    return file_handler
-
-
-class ColoredConsoleHandler(logging.StreamHandler):
-    COLORS = {
-        "TRACE": "\033[37m",  # Dark White (equivalent to gray)
-        "DEBUG": "\033[34m",  # Blue
-        "INFO": "\033[36m",  # Dark Cyan
-        "WARNING": "\033[33m",  # YELLOW
-        "ERROR": "\033[31m",  # RED
-        "CRITICAL": "\033[41m",  # BG RED
-    }
-
-    def emit(self, record):
-        # Set the color for this record
-        color_code = self.COLORS.get(record.levelname, "\033[0m")
-
-        # Format the record with color
-        formatted_record = color_code + self.format(record) + "\033[0m"  # \033[0m resets the color
-
-        # Print the colored record to console
-        print(formatted_record)
-
-
-def build_console_handler():
-    console_handler = ColoredConsoleHandler(stream=sys.stdout)
-    console_handler.setLevel(TRACE)
-    console_handler.setFormatter(default_logging_formatter)
-    return console_handler
-
-
-def configure_logging():
-    print(f"Setting up logging  {__file__}")
-
-    if len(logging.getLogger().handlers) == 0:
-        handlers = get_logging_handlers()
-        for handler in handlers:
-            if not handler in logging.getLogger("").handlers:
-                logging.getLogger("").handlers.append(handler)
-    else:
-        logger = logging.getLogger(__name__)
-        logger.info("Logging already configured")
+def configure_logging(level: LogLevel = LogLevel.INFO):
+    builder = LoggerBuilder(level)
+    builder.configure()
 
 
 def log_test_messages():
@@ -96,13 +99,13 @@ def log_test_messages():
     logger.trace("This is a TRACE message.")
     logger.debug("This is a DEBUG message.")
     logger.info("This is an INFO message.")
+    logger.success("This is a SUCCESS message.")
     logger.warning("This is a WARNING message.")
     logger.error("This is an ERROR message.")
     logger.critical("This is a CRITICAL message.")
 
-if __name__ == "__main__":
-    # configure the logging
-    configure_logging()
-    log_test_messages()
 
+if __name__ == "__main__":
+    configure_logging(LogLevel.TRACE)  # Setting the root logger level to TRACE
+    log_test_messages()
     print("Logging setup and tests completed. Check the console output and the log file.")
