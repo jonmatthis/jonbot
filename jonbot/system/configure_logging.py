a9772900035589440223103e336aa59e27f9b184
jonmatthis <jonmatthis@gmail.com>
2023-08-22 09:52:03 -0400

    add better Δt testing

--- jonbot/system/configure_logging.py
+++ jonbot/system/configure_logging.py
@@ -142,14 +142,15 @@ def log_test_messages(logger):
     print("----------This is a print message.------------------")
 
     import time
-    for iter in range(5):
+    for iter in range(1, 10):
+        wait_time = iter / 10
         print(f"Testing timestamps (round: {iter}:")
-        logger.info("Starting 1 sec timer")
+        logger.info("Starting 1 sec timer (Δt should probably be near 0, unless you've got other stuff going on)")
         tic = time.perf_counter_ns()
-        time.sleep(1)
+        time.sleep(wait_time)
         toc = time.perf_counter_ns()
         elapsed_time = (toc - tic) / 1e9
-        logger.info(f"Done 1 sec timer - elapsed time:{elapsed_time} (Δt should be ~1.0s)")
+        logger.info(f"Done {wait_time} sec timer - elapsed time:{elapsed_time} (Δt should be ~{wait_time}s)")
 
 
 if __name__ == "__main__":
