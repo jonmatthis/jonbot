4b309d0c718af8813466835be975774d4190ddd7
jonmatthis <jonmatthis@gmail.com>
2023-09-03 07:02:05 -0400

    convert memory to use gpt-3.5-turbo-16k in its summary chain

--- jonbot/models/memory_config.py
+++ jonbot/models/memory_config.py
@@ -13,7 +13,8 @@ CONVERSATION_HISTORY_MAX_TOKENS = 1000
 class ChatbotConversationMemoryConfig(BaseModel):
     memory_key: str = "chat_memory"
     input_key: str = "human_input"
-    llm: Any = OpenAI(temperature=0)
+    llm: Any = OpenAI(temperature=0,
+                      model_name="gpt-3.5-turbo-16k", )
     return_messages: bool = True
     max_token_limit: int = CONVERSATION_HISTORY_MAX_TOKENS
     summary_prompt: PromptTemplate = CONVERSATION_SUMMARY_PROMPT
