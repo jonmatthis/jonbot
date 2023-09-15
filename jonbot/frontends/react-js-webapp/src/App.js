d911fea1e2e15029e2bd83b4d957ec085e985847
Jon Matthis <jonmatthis@gmail.com>
2023-09-14 21:16:54 -0400

    poc json

--- jonbot/frontends/react-js-webapp/src/App.js
+++ jonbot/frontends/react-js-webapp/src/App.js
@@ -1,25 +1,37 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    fetch('manifest.json', {
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+    .then(response => response.json())
+    .then(jsonData => this.setState({ data: jsonData }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Data from JSON file:</h1>
+          <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
